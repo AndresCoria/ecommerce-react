ba075488f8a0276ecf810f6f4cc39f647061e931
Andres Coria <andrescoria1984@gmail.com>
2022-09-01 21:51:44 -0300

    Agregado de NavBar, Agregado de imagenes necesarias para el proyecto, Agregado CardWidget

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ItemListContainer.css'
+
+const ItemListContainer = () => {
+  return (
+    <div className="greeting">Bienvenidos a Zaion Cerveceria Artesanal</div>
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,40 @@
+import {Navbar, Nav, Container, NavDropdown} from "react-bootstrap";
+import Cardwidget from "../CardWidget";
+import Logo from "../Logo/Logo";
+
+const NavBar = () => {
+  return (
+    <div className="dNav" >
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  {/*<Navbar.Brand href="#home">Cerveceria Artesanal</Navbar.Brand>*/}
+  <Logo />
+  <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+  <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+    <Nav>
+      <Nav.Link href="#features">Inicio</Nav.Link>
+      <NavDropdown title="Productos" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Cervezas</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Tragos</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Hamburguesas</NavDropdown.Item>
+        {/*<NavDropdown.Divider />*/}
+        <NavDropdown.Item href="#action/3.4">Picadas</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Nosotros</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Contacto
+      </Nav.Link>
+    </Nav>
+    <Nav>
+  <Cardwidget />
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    </div>
+  )
+}
+
+export default NavBar
