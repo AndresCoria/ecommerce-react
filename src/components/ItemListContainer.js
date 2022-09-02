845864f2ea4e85b41e5aad231d4a1352d6824828
Andres Coria <andrescoria1984@gmail.com>
2022-09-02 10:57:25 -0300

    agregado de ItemListContainer para el desafio 3

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './ItemListContainer.css'
+import Titulo from './titulo/Titulo'
 
 const ItemListContainer = () => {
+
+  let greeting = "Bienvenidos a Zaion Cerveceria Artesanal"
+
   return (
-    <div className="greeting">Bienvenidos a Zaion Cerveceria Artesanal</div>
+    <div className="greeting">
+      <Titulo greeting={ greeting }/>
+    </div>
   )
 }
 
---
+++ src/components/titulo/Titulo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Titulo = ( { greeting } ) => {
+  return (
+    <div> { greeting } </div>
+  )
+}
+
+export default Titulo
